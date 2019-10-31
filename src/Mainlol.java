5fec082e66bb163483a60c0448a279207d659028
null0verflow <bobdinh139@gmail.com>
2019-10-30 21:39:37 -0400

    check jre version

--- src/Mainlol.java
+++ src/Mainlol.java
@@ -1,16 +1,21 @@
 
 
 public class Mainlol {
-  public static void main(String args[]) {
-      new Interface(false);
-	  CheckUpdate ccu= new CheckUpdate();
+	public static void main(String args[]) {
+		new Interface(false);
+		String version = System.getProperty("java.version");
+		System.out.println(version);
+        if (!version.equals("1.8.0_211")) {
+        	CheckUpdate.popUp("Please use java 8\nIf you don't, the code will not execute correctly!", "incompatible java version detected");
+        }
+		CheckUpdate ccu= new CheckUpdate();
 		try {
 			ccu.checkup();
 		} catch (Exception e) {
 			e.printStackTrace();
 
 		}
-	  
-  }
-	
+
+	}
+
 }
